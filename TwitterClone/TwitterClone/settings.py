6d8ac84b978027bd680633297c5c2c54be86df52
Caden Austin <cadenbryant@hotmail.com>
2021-02-28 22:54:42 -0600

    Removed Django-Heroku import in settings

--- TwitterClone/TwitterClone/settings.py
+++ TwitterClone/TwitterClone/settings.py
@@ -12,7 +12,6 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 import os
-import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -122,6 +121,3 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (str(BASE_DIR.joinpath('static')),)
 STATIC_ROOT = str(BASE_DIR.joinpath('staticfiles'))
-
-# Activate Django-Heroku.
-django_heroku.settings(locals())
